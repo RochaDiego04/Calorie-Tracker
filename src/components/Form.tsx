ee1d1e2d1d5c68f159ff47cd92b9743bd313f3e4
Diego Rocha <drochaesc@gmail.com>
2024-05-29 12:26:03 -0600

    Editing activities by clicking on the pencil icon, using useReducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,13 @@
-import { FormEvent, useState, Dispatch } from "react";
+import { FormEvent, useState, Dispatch, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import { categories } from "../data/categories";
 import { Activity } from "../types";
-import { ActivityActions } from "../reducers/activity-reducer";
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer";
 
 type FormProps = {
   dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
 };
 
 const initialState: Activity = {
@@ -16,8 +17,20 @@ const initialState: Activity = {
   calories: 0,
 };
 
-export default function Form({ dispatch }: FormProps) {
+export default function Form({ dispatch, state }: FormProps) {
   const [activity, setActivity] = useState<Activity>(initialState);
+
+  useEffect(() => {
+    if (state.activeId) {
+      // Filtrar las actividades usando el id activo.
+      // Filter retorna un arreglo, como solo deberíamos encontrar un unico id, asignamos a la variable la posicion 0.
+      const selectActivity = state.activities.filter(
+        (stateActivity) => stateActivity.id === state.activeId
+      )[0];
+      // seteamos el state del formulario
+      setActivity(selectActivity);
+    }
+  }, [state]);
 
   const handleChange = (
     e:
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -3,15 +3,18 @@ import { Activity } from "../types";
 export type ActivityActions =
   // payload son los datos que se agregarán al state
   // podemos agregar mas actions con |
-  { type: "save-activity"; payload: { newActivity: Activity } };
+  | { type: "save-activity"; payload: { newActivity: Activity } }
+  | { type: "set-active-id"; payload: { id: Activity["id"] } };
 
-type ActivityState = {
+export type ActivityState = {
   activities: Activity[];
+  activeId: Activity["id"]; // en lugar de poner tipo de dato string, lo ponemos así por si luego cambiamos el id a int, etc. se actualice solo
 };
 
 export const initialState: ActivityState = {
   // El hook inicia como arreglo vacío
   activities: [],
+  activeId: "",
 };
 
 // Se encarga de conectar las acciones y el state
@@ -20,11 +23,29 @@ export const activityReducer = (
   action: ActivityActions
 ) => {
   if (action.type === "save-activity") {
+    let updatedActivities: Activity[] = [];
+    // si hay un activeId, es que estamos editando
+    if (state.activeId) {
+      updatedActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updatedActivities = [...state.activities, action.payload.newActivity];
+    }
+
     // Aqui se maneja la logica para actualizar el state
     // usamos spread ... por si tuvieramos mas cosas en initialState ademas del arreglo []
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity],
+      activities: updatedActivities,
+      activeId: "",
+    };
+  }
+
+  if (action.type === "set-active-id") {
+    return {
+      ...state,
+      activeId: action.payload.id,
     };
   }
 
